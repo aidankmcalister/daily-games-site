c5f5a55d411f5ab6770c639b3b7827f1df920b83
Aidan McAlister <aidankmcalister@gmail.com>
2026-01-08 16:50:42 -0500

    vitest update

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+import React, { act } from "react";
+
+// Polyfill for React 19 compatibility where some tools might still expect React.act
+if (!React.act) {
+  (React as any).act = act;
+}
