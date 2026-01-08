b3dfc636d4fdb2ed5c1718da9a8ad71a2fbcd3a8
Aidan McAlister <aidankmcalister@gmail.com>
2026-01-08 16:52:52 -0500

    test build

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,20 @@
 import "@testing-library/jest-dom";
-import React, { act } from "react";
+import { act } from "react";
+import * as React from "react";
 
-// Polyfill for React 19 compatibility where some tools might still expect React.act
-if (!React.act) {
+// Polyfill for React 19 compatibility
+// Many testing libraries expect React.act to be available from the 'react' default export
+// or the global React object.
+
+// 1. Shim on the default export
+if (React && !(React as any).act) {
   (React as any).act = act;
+}
+
+// 2. Ensure global usage works if environment is set up that way
+if (typeof globalThis !== "undefined" && !(globalThis as any).React) {
+  (globalThis as any).React = React;
+}
+if ((globalThis as any).React && !(globalThis as any).React.act) {
+  (globalThis as any).React.act = act;
 }
